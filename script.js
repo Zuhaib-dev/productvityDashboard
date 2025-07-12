5a3b546580ffe5dcc7929594b3a914a7546019bd
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-07-12 11:33:42 +0530

    Refactor HTML structure and enhance CSS styles for improved layout and responsiveness; update JavaScript for feature interaction

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-var allElems = document.querySelectorAll('.elem')
-var allFullElems = document.querySelectorAll('.fullElem')
- 
-allElems.forEach(function(elem){
-    elem.addEventListener('click',function(){
-       allFullElems[elem.id].style.display = 'block'
-       
-    })
-    
-    
-    
-    
+function openFeatures() {
+  var allElems = document.querySelectorAll(".elem");
+  var fullELemPage = document.querySelectorAll(".fullElem");
+  var allFullElemsBackBtn = document.querySelectorAll(".fullElem .back");
 
-    
-})
+  allElems.forEach(function (elem) {
+    elem.addEventListener("click", function () {
+      fullELemPage[elem.id].style.display = "block";
+    });
+  });
+  allFullElemsBackBtn.forEach(function (back) {
+    back.addEventListener("click", function () {
+      fullELemPage[back.id].style.display = "none";
+    });
+  });
+}
+openFeatures();
